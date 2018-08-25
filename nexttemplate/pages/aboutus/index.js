35fb9a71d154fbcb8386eab4b35a92f1aac3c0ba
divine <divinecutler@gmail.com>
2018-08-25 23:47:09 +0530

    updated

---
+++ nexttemplate/pages/aboutus/index.js
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+
+class IndexPage extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+                <h1>HomePage</h1>
+            </div>
+        )
+    }
+}
+export default IndexPage;
