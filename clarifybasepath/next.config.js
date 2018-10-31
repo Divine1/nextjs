2ff0175bd941f779566506ad23af9a4fa93037bb
divine <cdivine304@gmail.com>
2018-10-31 08:12:13 +0530

    update

--- clarifybasepath/next.config.js
+++ clarifybasepath/next.config.js
@@ -1,5 +1,10 @@
 // next.config.js
 module.exports = {
     useFileSystemPublicRoutes: false,
-    assetPrefix : "/dave"
+    assetPrefix : "/dave",
+    publicRuntimeConfig: { // Will be available on both server and client
+      staticFolder: '/dave/static'
+    }
   }
+
+  //https://zeit.co/blog/next5-1
--- clarifybasepath/pages/index.js
+++ clarifybasepath/pages/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Router ,{withRouter}from 'next/router';
-
+import getConfig from 'next/config'
+const {serverRuntimeConfig, publicRuntimeConfig} = getConfig()
 
 
 class IndexPage extends Component{
@@ -8,12 +9,16 @@ class IndexPage extends Component{
 
     componentDidMount(){
         console.log("in componentDidMount IndexPage")
-        Router.push({pathname: '/dave/home'})
+        //Router.push({pathname: '/dave/home'})
     }
     render(){
         return(
             <div>
                 from IndexPage
+                <div>
+                <img src={`${publicRuntimeConfig.staticFolder}/images/pexels-photo-213958.jpeg`}
+                alt="img not found" />
+                </div>
             </div>
         )
     }
